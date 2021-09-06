1ee54b17ba52756cb1add962e10dea32d14d17d0
Asko Nõmm <asko@bien.ee>
2021-09-06 15:00:08 -0300

    Optionally global

--- installer.sh
+++ installer.sh
@@ -4,10 +4,19 @@ if [ "$(uname)" == "Darwin" ]; then
   curl -O -L https://github.com/askonomm/babe/releases/latest/download/babe-macos && \
   mv babe-macos babe && \
   chmod +x babe
-  sudo mv babe /usr/local/bin/babe
 else
   curl -O -L https://github.com/askonomm/babe/releases/latest/download/babe-linux && \
   mv babe-linux babe && \
   chmod +x babe
+fi
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    -g|--global) global="true"; shift ;;
+  esac
+  shift
+done
+
+if [ "$global" == "true" ]; then
   sudo mv babe /usr/local/bin/babe
 fi
