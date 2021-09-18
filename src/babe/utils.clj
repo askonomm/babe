2357be2aa1c18e41f4507514a145b65c22313d5b
Asko Nõmm <asko@bien.ee>
2021-09-18 00:34:03 -0300

    Buy into Tonsky's idea of better readability

--- src/babe/utils.clj
+++ src/babe/utils.clj
@@ -14,6 +14,7 @@
         (triml trim-char))
     string))
 
+
 (defn trimr
   "Trims the given `trim-char` from the right of `string`."
   [string trim-char]
@@ -22,12 +23,14 @@
         (trimr trim-char))
     string))
 
+
 (defn- parse-md-metadata-value-by-key
   "Parses YAML metadata values depending on key."
   [value key]
   (cond (= "date" key)
         (inst/read-instant-date value)
         :else value))
+
 
 (defn- parse-md-metadata-line
   "Parses each YAML metadata line into a map."
@@ -36,6 +39,7 @@
         value (str/trim (second (str/split line #":")))]
     {(keyword key)
      (parse-md-metadata-value-by-key value key)}))
+
 
 (defn parse-md-metadata
   "Takes in a given `content` as the entirety of a Markdown
@@ -46,6 +50,7 @@
       (into {} (map #(parse-md-metadata-line %) lines)))
     {}))
 
+
 (defn parse-md-entry
   "Takes in a given `content` as the entirety of a Markdown
   content file, and then parses the Markdown into HTML from it."
@@ -55,23 +60,25 @@
       (str/trim)
       (md/md-to-html-string)))
 
+
 (defn scan
   "Scans a given `directory` according to `when-pred`, and
   returns a list of maps containing the path of a file and
   the modified time of said file."
   [directory when-pred]
   (flatten
-    (for
-      [file (.list (io/file directory))
-       :let [read-dir (trimr directory "/")
-             path (str read-dir "/" file)]
-       :when (when-pred read-dir (str/lower-case path))]
-      (if (.isDirectory (io/file path))
-        (scan path when-pred)
-        {:path  (-> path
-                    (triml "/")
-                    (trimr "/"))
-         :mtime (.lastModified (io/file path))}))))
+   (for
+    [file (.list (io/file directory))
+     :let [read-dir (trimr directory "/")
+           path (str read-dir "/" file)]
+     :when (when-pred read-dir (str/lower-case path))]
+     (if (.isDirectory (io/file path))
+       (scan path when-pred)
+       {:path  (-> path
+                   (triml "/")
+                   (trimr "/"))
+        :mtime (.lastModified (io/file path))}))))
+
 
 (defn delete-files-in-path!
   "Deletes all files and folders from within the given `path`,
@@ -81,6 +88,7 @@
     (if (.isDirectory ^File file-in-dir)
       (delete-files-in-path! (.getPath ^File file-in-dir))
       (io/delete-file file-in-dir))))
+
 
 (defn argcmd
   "Parses a given list of `args` for a `command` and returns
