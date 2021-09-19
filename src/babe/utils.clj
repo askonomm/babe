6767a2e04179578efb64c449776596ee261bdd86
Asko Nõmm <asko@bien.ee>
2021-09-19 19:28:30 -0300

    Implement #3

--- src/babe/utils.clj
+++ src/babe/utils.clj
@@ -1,15 +1,16 @@
 (ns babe.utils
-  (:require [clojure.string :as str]
+  (:require [clojure.string :as string]
             [clojure.instant :as inst]
             [clojure.java.io :as io]
             [markdown.core :as md])
   (:import (java.io File)
            (clojure.lang PersistentList)))
 
+
 (defn triml
   "Trims the given `trim-char` from the left of `string`."
   [string trim-char]
-  (if (clojure.string/starts-with? string trim-char)
+  (if (string/starts-with? string trim-char)
     (-> (subs string 1)
         (triml trim-char))
     string))
@@ -18,7 +19,7 @@
 (defn trimr
   "Trims the given `trim-char` from the right of `string`."
   [string trim-char]
-  (if (clojure.string/ends-with? string trim-char)
+  (if (string/ends-with? string trim-char)
     (-> (subs string 0 (- (count string) 1))
         (trimr trim-char))
     string))
@@ -35,8 +36,8 @@
 (defn- parse-md-metadata-line
   "Parses each YAML metadata line into a map."
   [line]
-  (let [key (str/trim (first (str/split line #":")))
-        value (str/trim (second (str/split line #":")))]
+  (let [key (string/trim (first (string/split line #":")))
+        value (string/trim (second (string/split line #":")))]
     {(keyword key)
      (parse-md-metadata-value-by-key value key)}))
 
@@ -46,7 +47,7 @@
   content file, and then parses YAML metadata from it."
   [contents]
   (if-let [match (re-find #"(?s)^(---)(.*?)(---|\.\.\.)" contents)]
-    (let [lines (remove #(= "---" %) (str/split-lines (first match)))]
+    (let [lines (remove #(= "---" %) (string/split-lines (first match)))]
       (into {} (map #(parse-md-metadata-line %) lines)))
     {}))
 
@@ -56,8 +57,8 @@
   content file, and then parses the Markdown into HTML from it."
   [contents]
   (-> contents
-      (str/replace #"(?s)^---(.*?)---*" "")
-      (str/trim)
+      (string/replace #"(?s)^---(.*?)---*" "")
+      (string/trim)
       (md/md-to-html-string)))
 
 
@@ -71,7 +72,7 @@
     [file (.list (io/file directory))
      :let [read-dir (trimr directory "/")
            path (str read-dir "/" file)]
-     :when (when-pred read-dir (str/lower-case path))]
+     :when (when-pred read-dir (string/lower-case path))]
      (if (.isDirectory (io/file path))
        (scan path when-pred)
        {:path  (-> path
